8977dd447393a13edc8874df894250d239870a61
Aleksey Zaytcev <aleksey2142.zaytcev@gmail.com>
2019-08-22 23:36:46 +0300

    Setup Linters

---
+++ .overcommit_gems.rb
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '2.6.3'
+
+gem 'overcommit', '~> 0.49.1'
+gem 'bundler-audit', '~> 0.6.1'
+gem 'fasterer', '~> 0.6.0'
+gem 'reek', '~> 5.4'
+gem 'rubocop-rails', '~> 2.3'
